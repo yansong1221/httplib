c6e0b9dde1ca0af7d9b98b6c104cf07f6d5f1c6a
yansong <yansong1221@hotmail.com>
2026-09-24 15:56:03 +0800

    refactor(body): 解压/压缩抽成 compressed_body<Inner> 包装，any_body 收敛为内联转发

--- lib/body/any_body.hpp
+++ lib/body/any_body.hpp
@@ -1,12 +1,12 @@
 #pragma once
 #include "body/buffer_body.hpp"
+#include "body/compressed_body.hpp"
 #include "body/empty_body.hpp"
 #include "body/file_body.hpp"
 #include "body/form_data_body.hpp"
 #include "body/json_body.hpp"
 #include "body/query_params_body.hpp"
 #include "body/string_body.hpp"
-#include "compress/compressor.hpp"
 #include "httplib/config.hpp"
 #include <tuple>
 #include <type_traits>
@@ -14,6 +14,7 @@
 
 namespace httplib::body
 {
+
     struct any_body
     {
         template <typename... Bodies>
@@ -46,8 +47,15 @@ namespace httplib::body
             }
         };
 
-        using body_types
-            = std::tuple<empty_body, string_body, json_body, form_data_body, file_body, query_params_body, buffer_body>;
+        // 每个 body 都套一层 compressed_body：按 Content-Encoding 透明编解码，
+        // 未知/identity 编码时退化为纯转发。value_type 保持不变，故对外 API 不受影响。
+        using body_types = std::tuple<compressed_body<empty_body>,
+                                      compressed_body<string_body>,
+                                      compressed_body<json_body>,
+                                      compressed_body<form_data_body>,
+                                      compressed_body<file_body>,
+                                      compressed_body<query_params_body>,
+                                      compressed_body<buffer_body>>;
 
         template <typename>
         struct value_from_tuple;
@@ -63,7 +71,6 @@ namespace httplib::body
         class writer;
         class reader;
     };
-
     namespace detail
     {
         template <typename T, typename... Bodies>
@@ -187,9 +194,10 @@ namespace httplib::body
 
         template <typename Body>
         void
-        emplace(http::fields& h, typename Body::value_type& b)
+        emplace(http::fields& h, typename Body::value_type& b, std::uint64_t limit = 0)
         {
-            v.template emplace<typename Body::reader>(h, b);
+            auto& r = v.template emplace<typename Body::reader>(h, b);
+            r.set_limit(limit);
         }
     };
 
@@ -205,10 +213,20 @@ namespace httplib::body
             , body_(b)
         {
         }
-        explicit writer(http::fields& h, value_type& b);
+        explicit writer(http::fields& h, value_type& b) : header_(h), body_(b) {}
 
-        void init(boost::system::error_code& ec);
-        boost::optional<std::pair<const_buffers_type, bool>> get(boost::system::error_code& ec);
+        void
+        init(boost::system::error_code& ec)
+        {
+            create_writer(header_, body_);
+            proxy_.init(ec);
+        }
+
+        boost::optional<std::pair<const_buffers_type, bool>>
+        get(boost::system::error_code& ec)
+        {
+            return proxy_.get(ec);
+        }
 
       private:
         template <typename... Bodies>
@@ -231,7 +249,6 @@ namespace httplib::body
         http::fields& header_;
         value_type& body_;
         writer_holder proxy_;
-        compress::compressor::ptr compressor_;
     };
 
     class any_body::reader
@@ -246,48 +263,84 @@ namespace httplib::body
             , body_(b)
         {
         }
-        explicit reader(http::fields& h, value_type& b);
+        explicit reader(http::fields& h, value_type& b) : header_(h), body_(b) {}
 
-        void init(boost::optional<std::uint64_t> const& content_length, boost::system::error_code& ec);
-        std::size_t put(const_buffers_type const& buffers, boost::system::error_code& ec);
-        void finish(boost::system::error_code& ec);
+        void
+        init(boost::optional<std::uint64_t> const& content_length, boost::system::error_code& ec)
+        {
+            auto content_type = header_[http::field::content_type];
+            // 必须在 create_reader 之前取：create_reader 可能重设 body_（赋值
+            // variant_value 会把 decompressed_limit 一并置 0）。
+            auto limit = body_.decompressed_limit;
+
+            if (!std::holds_alternative<body::empty_body::value_type>(body_))
+            {
+                create_reader(body_, limit);
+            }
+            else if (content_type.starts_with("multipart/form-data"))
+            {
+                create_reader<form_data_body>(limit);
+            }
+            else if (content_type.starts_with("application/json"))
+            {
+                create_reader<json_body>(limit);
+            }
+            else if (content_type.starts_with("application/x-www-form-urlencoded"))
+            {
+                create_reader<query_params_body>(limit);
+            }
+            else
+            {
+                create_reader<string_body>(limit);
+            }
+            proxy_.init(content_length, ec);
+        }
+
+        std::size_t
+        put(const_buffers_type const& buffers, boost::system::error_code& ec)
+        {
+            return proxy_.put(buffers, ec);
+        }
+
+        void
+        finish(boost::system::error_code& ec)
+        {
+            proxy_.finish(ec);
+        }
 
       private:
         template <typename... Bodies>
         void
-        create_reader(any_body::variant_value<Bodies...>& body)
+        create_reader(any_body::variant_value<Bodies...>& body, std::uint64_t limit)
         {
             std::visit(
-                [this](auto& t)
+                [&](auto& t)
                 {
                     using value_type = std::decay_t<decltype(t)>;
                     using body_type = typename detail::match_body<value_type, Bodies...>::type;
                     static_assert(!std::is_void_v<body_type>, "No matching Body type found");
 
-                    proxy_.template emplace<body_type>(header_, t);
+                    proxy_.template emplace<body_type>(header_, t, limit);
                 },
                 body);
         }
 
         template <class Body>
         void
-        create_reader()
+        create_reader(std::uint64_t limit)
         {
             if (!std::holds_alternative<typename Body::value_type>(body_))
             {
                 body_ = typename Body::value_type {};
             }
 
-            proxy_.template emplace<Body>(header_, std::get<typename Body::value_type>(body_));
+            proxy_.template emplace<compressed_body<Body>>(header_, std::get<typename Body::value_type>(body_), limit);
         }
 
       private:
         http::fields& header_;
         value_type& body_;
         reader_holder proxy_;
-        compress::compressor::ptr compressor_;
-        std::uint64_t decompressed_limit_ = 0;
-        std::uint64_t decompressed_bytes_ = 0;
     };
 
 } // namespace httplib::body
